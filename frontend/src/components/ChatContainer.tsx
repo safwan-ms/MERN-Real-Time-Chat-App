cbb7c4d9b286328e6258fbb54b964400557fd7b1
safwan-ms <safwan.anasms@gmail.com>
2025-05-25 22:47:56 +0530

    fix: Enhance ChatContainer with auto-scroll using typed useRef and scrollIntoView

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,5 +1,6 @@
-import assets from "@/assets/assets";
+import assets, { messagesDummyData } from "@/assets/assets";
 import type { DummyDataProps } from "@/types";
+import { useEffect, useRef } from "react";
 
 interface ChatContainerProps {
   selectedUser: DummyDataProps | null;
@@ -10,8 +11,16 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
   selectedUser,
   setSelectedUser,
 }) => {
+  const scrollEnd = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return selectedUser ? (
     <main className="h-full overflow-scroll relative backdrop:blur-lg">
+      {/* ----------------header-------------------- */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img
           src={assets.profile_martin}
@@ -33,6 +42,50 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
           alt="help_icon"
           className="max-md:hidden max-w-5"
         />
+      </div>
+
+      {/* ---------------chat area----------------- */}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3  pb-6 ">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${
+              msg.senderId !== "680f50aaf10f3cd28382ecf2" && "flex-row-reverse"
+            }`}
+          >
+            {msg.image ? (
+              <img
+                src={msg.image}
+                alt="profile"
+                className="max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId === "680f50aaf10f3cd28382ecf2"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
+                {msg.text}
+              </p>
+            )}
+
+            <div className="text-center text-xs ">
+              <img
+                src={
+                  msg.senderId === "680f50aaf10f3cd28382ecf2"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                alt="profile"
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500"> {msg.createdAt}</p>
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}></div>
       </div>
     </main>
   ) : (
