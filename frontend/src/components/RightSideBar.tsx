f48995f4bfda08fd5d1646868c4826347777345e
safwan-ms <safwan.anasms@gmail.com>
2025-05-26 09:15:44 +0530

    feat: Add RightSideBar component displaying selected user profile and media gallery

--- frontend/src/components/RightSideBar.tsx
+++ frontend/src/components/RightSideBar.tsx
@@ -1,3 +1,4 @@
+import assets, { imagesDummyData } from "@/assets/assets";
 import type { DummyDataProps } from "@/types";
 
 interface RightSidebarProps {
@@ -10,9 +11,46 @@ const RightSideBar: React.FC<RightSidebarProps> = ({
   setSelectedUser,
 }) => {
   return (
-    <div>
-      <h1>RightSideBar</h1>
-    </div>
+    selectedUser && (
+      <div
+        className={`bg-[#8185b2]/10 text-white w-full relative overflow-y-scroll ${
+          selectedUser ? "max-md:hidden" : ""
+        }`}
+      >
+        <div className="pt-16 flex items-center flex-col gap-2 text-xs font-light mx-auto">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt="Profile_pic"
+            className="w-20 aspect-[1/1] rounded-full"
+          />
+          <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
+            <span className="w-2 h-2 rounded-full bg-green-500"> </span>
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto text-center">{selectedUser.bio}</p>
+        </div>
+
+        <hr className="border-[#ffffff]/50 my-4 " />
+
+        <div className="px-5 text-xs">
+          <p>Media</p>
+          <div className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80 ">
+            {imagesDummyData.map((url, index) => (
+              <div
+                key={index}
+                onClick={() => window.open(url)}
+                className="cursor-pointer rounded "
+              >
+                <img src={url} alt="Url" className="h-full rounded-md" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+          Logout
+        </button>
+      </div>
+    )
   );
 };
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   const [selectedUser, setSelectedUser] = useState<DummyDataProps | null>(null);
 
   return (
-    <div className="w-full h-screen sm:px-[15%] sm:py-[5%] ">
+    <div className="w-full h-screen sm:px-[10%] sm:py-[3%] ">
       <div
         className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${
           selectedUser
