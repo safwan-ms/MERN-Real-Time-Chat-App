b64837f256505b511ba910bd8150f98e164b5cc4
safwan-ms <safwan.anasms@gmail.com>
2025-05-27 19:12:12 +0530

    feat: Add MongoDB connection, user model, and signup controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,43 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password, bio } = req.body;
+  try {
+    if (!fullName || !email || !password || !bio) {
+      res.json({ success: false, message: "All fields are required!" });
+    }
+
+    const checkUserExists = await User.findOne({ email });
+
+    if (checkUserExists) {
+      return res.json({ success: false, message: "User already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      bio,
+    });
+
+    const token = generateToken(newUser._id);
+
+    res.json({
+      success: true,
+      userData: newUser,
+      token,
+      message: "Account created successfully",
+    });
+  } catch (error) {
+    console.log("Internal Server error", error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
+import http from "http";
+import connectDB from "./lib/db.js";
 dotenv.config();
-const PORT = process.env.PORT;
-console.log(PORT);
-const app = express();
 
-app.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
+//Connect to db
+connectDB();
+
+const app = express();
+const server = http.createServer(app);
+
+//Middleware setup
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+app.use("/api/status", (req, res) => res.send("Server is live"));
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
