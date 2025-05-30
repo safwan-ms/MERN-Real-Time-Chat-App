033d28fd98e78280c87cb32d9f2b5e0f676c88dc
safwan-ms <safwan.anasms@gmail.com>
2025-05-30 17:59:15 +0530

    feat: Add Cloudinary upload and delete utilities with streamifier support

---
+++ backend/helpers/cloudinaryHelper.js
@@ -0,0 +1,35 @@
+import cloudinary from "../lib/cloudinary";
+import streamifier from "streamifier";
+
+const uploadToCloudinary = async (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" },
+      (error, result) => {
+        if (error) {
+          console.error("Cloudinary  upload failed: ", error);
+          return reject(
+            new Error(`Cloudinary upload failed: ${error.message}`)
+          );
+        }
+        const { secure_url: url, public_id: publicId } = result;
+        resolve(result);
+      }
+    );
+
+    // Pipe the buffer to the Cloudinary stream
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
+
+const removeFromCloudinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    return result;
+  } catch (error) {
+    console.error("Error while removing from Cloudinary", error);
+    throw new Error("Error while removing from Cloudinary");
+  }
+};
+
+export { uploadToCloudinary, removeFromCloudinary };
